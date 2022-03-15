2f5281226822873a2c0ccbbc1af7baa041e13a45
wang.haoyu <228233464@qq.com>
2022-03-15 22:03:36 +0800

    merge koa

--- koa/index.js
+++ koa/index.js
@@ -4,19 +4,17 @@ const server1 = new Koa();
 const server2 = new Koa();
 
 server1.use((ctx) => {
-  console.log(ctx.request.path);
-  console.log(ctx.request.url, 'url');
-  console.log(ctx.request, 'url');
+  ctx.response.body = 'ok';
 });
 
 server2.use((ctx) => {
   console.log(ctx.name, 'server 2');
 });
 
-server1.listen(3001, () => {
-  console.log('server1 on 3001');
+server1.listen(2998, () => {
+  console.log('server1 on 2998');
 });
 
-server2.listen(3002, () => {
-  console.log('server2 on 3002');
+server2.listen(2999, () => {
+  console.log('server2 on 2999');
 });
