0702b789c83f3c2ac4b58e41a6e4d13230e415c7
wang.haoyu <228233464@qq.com>
2022-04-07 17:00:30 +0800

    feat(vue): h 方法

---
+++ vue/packages/runtime-core/src/h.ts
@@ -0,0 +1,36 @@
+import { isArray, isPlainObj } from '@vue/share';
+import { createVNode, isVNode } from './vnode';
+
+/**
+ * h日常传递参数种类
+ * 1. h('div')
+ * 2. h('div',{ style:{ color:'red' } })
+ * 3. h('div',{ style:{ color:'red' } },'hello')
+ * 4. h('div','hello')
+ * 5. h('div',null, 'hello','world')
+ * 6. h('div',h('span'))
+ * 7. h('div',[h('span')])
+ */
+export function h(type, propsOrChildren, children) {
+  const length = arguments.length;
+  // 两个参数
+  if (length === 2) {
+    if (isPlainObj(propsOrChildren) && !isArray(propsOrChildren)) {
+      // 第二个参数是属性 || 非数组Vnode
+      if (isVNode(propsOrChildren)) {
+        return createVNode(type, null, [propsOrChildren]);
+      }
+      return createVNode(type, propsOrChildren);
+    } else {
+      // 第二个参数非对象 有可能是字符串
+      createVNode(type, null, propsOrChildren);
+    }
+  } else {
+    if (length > 3) {
+      children = Array.from(arguments).slice(3);
+    } else if (length === 3 && isVNode(children)) {
+      children = [children];
+    }
+    return createVNode(type, propsOrChildren, children);
+  }
+}
---
+++ vue/packages/runtime-core/src/vnode.ts
@@ -0,0 +1,35 @@
+// 表示当前vnode类型 type
+
+import { isArray, isString, ShapeFlags } from '@vue/share';
+
+export function isVNode(value: any) {
+  return value ? value.__v_isVNode === true : false;
+}
+
+/**
+ * 底层根据type，prop，child创建VNode h方法的底层调用
+ * @param type 类型
+ * @param prop 属性
+ * @param child 儿子
+ */
+export function createVNode(type, props, children = null) {
+  // 当前节点类型
+  let shapeFlag = isString(type) ? ShapeFlags.ELEMENT : 0;
+
+  const vnode = {
+    type,
+    props,
+    children,
+    key: props?.key,
+    el: null, // diff算法会匹配根据当前Vnode挂载的元素进行对应属性/值替换，而不会重新生成元素
+    shapeFlag,
+    __v_isVNode: true,
+  };
+
+  // 儿子节点类型
+  if (children) {
+    vnode.shapeFlag |= isString(children)
+      ? ShapeFlags.TEXT_CHILDREN
+      : ShapeFlags.ARRAY_CHILDREN;
+  }
+}
