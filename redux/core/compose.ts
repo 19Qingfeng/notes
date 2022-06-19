ba0ac68accf42fcfc8634f1bb25e23654634bf55
wang.haoyu <228233464@qq.com>
2022-06-19 15:49:22 +0800

    feat(redux): compose

---
+++ redux/core/compose.ts
@@ -0,0 +1,21 @@
+const compose = (...fns) => {
+  return fns.reduce((a, b) => (...args) => a(b(...args)))
+}
+
+
+export default function compose(...funcs: Function[]) {
+  if (funcs.length === 0) {
+    // infer the argument type so it is usable in inference down the line
+    return <T>(arg: T) => arg
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  return funcs.reduce(
+    (a, b) =>
+      (...args: any) =>
+        a(b(...args))
+  )
+}
